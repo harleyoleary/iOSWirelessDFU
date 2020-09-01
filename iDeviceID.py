1b3a43c40db073f0b0c2e1025f063e5d3d0da973
harleyoleary <70482723+harleyoleary@users.noreply.github.com>
2020-09-01 01:02:38 +0100

    Gathers Device Information

---
+++ iDeviceID.py
@@ -0,0 +1,13 @@
+ wget;deviceinfo
+  wget;deviceid
+
+if wget value
+ == {deviceid} + {deviceinfo}
+  then exit() else
+   proceed() 
+    cd:/iOSWirelessDFU/USBMAP.PY
+    
+                 #### USER NOTE ####
+      ** Change Directory to Installation Folder **
+
+
